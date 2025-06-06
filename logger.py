8500a7d0ef3927ec10d84dd1b263f1c51cd5ae02
galvesrod <galves.rod@outlook.com>
2025-06-06 10:11:36 -0300

    criação classe de logger

---
+++ logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+class Logger:
+    def __init__(self):        
+        self.path = r".\logs"        
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+        self.logger = logging.getLogger('gravador')        
+
+        logging.basicConfig(
+            filename= self.path + rf'\logs.log',
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+
+        logging.getLogger("paramiko").setLevel(logging.CRITICAL)
+        logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+        logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+
+    def info(self, msg:str):
+        self.logger.info(f"{msg}")
+
+    def erro(self, msg:str):
+        self.logger.info(f"{msg}")
+
+    def warning(self, msg:str):
+        self.logger.info(f"{msg}")
