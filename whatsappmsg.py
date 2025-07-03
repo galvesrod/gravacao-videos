27d4282ff990d0c4ff3961a1de35c4a8f9aba358
galvesrod <galves.rod@outlook.com>
2025-07-03 11:53:48 -0300

    correção nas configurações de inicialização do chrome option

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -50,7 +50,7 @@ class WhatsAppWeb:
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         
         # Configurações de janela
-        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--start-minimized")
         # chrome_options.add_argument("--window-size=1920,1080")
         
         # Configurações importantes para persistência de sessão
@@ -58,10 +58,9 @@ class WhatsAppWeb:
         chrome_options.add_argument("--enable-cookies")
         chrome_options.add_argument("--allow-running-insecure-content")
 
-        chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument('--disable-logging')
         chrome_options.add_argument("--log-level=3")
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
 
         # Desabilitar som/áudio
         chrome_options.add_argument("--mute-audio")
