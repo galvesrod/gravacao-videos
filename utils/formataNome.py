e1fcfff8649cb89b5c7f3ff239d6b86efe53be74
galvesrod <galves.rod@outlook.com>
2025-07-22 07:23:44 -0300

    Correção ao gravar um diretório com caracteres especiais

--- utils/formataNome.py
+++ utils/formataNome.py
@@ -1,17 +1,22 @@
 
 from typing import Literal
+import re
 
 
 def formataNome(nome:str, type: Literal['DIR', 'FL'] = 'FL')-> str:
     # ignorar: \/:*?"<>|
+
+    primeiro_dois_pontos = nome.find(':')
+    nome =  nome[:primeiro_dois_pontos+1] + nome[primeiro_dois_pontos+1:].replace(':','')
     nome = nome.replace('/','')
-    nome = nome.replace(':','')
     nome = nome.replace('*','')
     nome = nome.replace('\"','\'')
     nome = nome.replace('?','')
     nome = nome.replace('>','')
     nome = nome.replace('<','')
+    
     if type == 'FL':
         nome = nome.replace('\\','')
-        
+    
+            
     return nome
