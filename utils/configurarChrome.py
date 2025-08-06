202567ef0e046399eb73f130b740f7782e0dfeb0
galvesrod <galves.rod@outlook.com>
2025-08-06 14:06:41 -0300

    inclusão metodo para deixar video mudo

--- utils/configurarChrome.py
+++ utils/configurarChrome.py
@@ -2,7 +2,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
-def configurarChrome(headless:bool=False) -> webdriver:
+def configurarChrome(headless:bool=False, muted:bool=False) -> webdriver:
     #  Configurar Selenium
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
@@ -13,9 +13,23 @@ def configurarChrome(headless:bool=False) -> webdriver:
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     if headless:
         chrome_options.add_argument("--headless")  
+
+    # Configurações para desabilitar áudio
+    if muted:
+        print('mudo')
+        chrome_options.add_argument("--mute-audio")
+        chrome_options.add_argument("--disable-audio")
+        chrome_options.add_argument("--disable-audio-output")
+        chrome_options.add_argument("--disable-background-media-suspend")
+        chrome_options.add_argument("--disable-background-timer-throttling")
+        chrome_options.add_argument("--disable-renderer-backgrounding")
+        chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+        chrome_options.add_argument("--disable-features=VizDisplayCompositor")
+        chrome_options.add_argument("--autoplay-policy=document-user-activation-required")
     service = Service('chromedriver.exe')  # caminho do chromedriver
     
     page = webdriver.Chrome(service=service, options=chrome_options)
     page.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
     page.implicitly_wait(30)
+
     return page
